aad31acde8743a8a9912fa92faa5f4c3da512a18
Guadagno <jguadagno@hotmail.com>
2020-02-27 13:32:49 -0700

    Hello World - Width Flexbox

--- ReactNativeEvent/App.js
+++ ReactNativeEvent/App.js
@@ -5,13 +5,14 @@ import {
   StyleSheet
 } from 'react-native';
 
-export default class FixedDimensionsBasics extends Component {
+export default class FlexDirectionBasics extends Component {
   render() {
     return (
-      <View>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+      // Try setting `flexDirection` to `column`.
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
