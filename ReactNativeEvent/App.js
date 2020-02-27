246c5f2eb941dc18e922d27ed3b5adf26d4ea3b6
Guadagno <jguadagno@hotmail.com>
2020-02-26 17:32:29 -0700

    Hello World - Styles

--- ReactNativeEvent/App.js
+++ ReactNativeEvent/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import {
   Text,
-  View
+  View,
+  StyleSheet
 } from 'react-native';
 
 export default class HelloWorld extends Component {
   render() {
     return (
-      <View>
-        <Text>
-          Hello World
+      <View style={styles.center}>
+        <Text style={styles.red}>
+          Hello Event_Name
         </Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  red: {
+    color: 'red',
+  },
+  center: {
+    alignItems: 'center',
+    paddingTop: 30, // For iOS
+  }
+});
