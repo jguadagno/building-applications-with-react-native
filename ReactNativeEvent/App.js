e9631ca322ee355472d3f7fbdf2a66e965e2e9dd
Guadagno <jguadagno@hotmail.com>
2020-02-27 13:39:34 -0700

    Hello World - Text Input

--- ReactNativeEvent/App.js
+++ ReactNativeEvent/App.js
@@ -1,23 +1,28 @@
-import React, { Component } from 'react';
-import {
-  Text,
-  View,
-  StyleSheet
-} from 'react-native';
+import React, {Component} from 'react';
+import {Text, TextInput, View} from 'react-native';
 
-export default class FlexDirectionBasics extends Component {
+export default class FlagTranslator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
   render() {
     return (
-    <View style={{
-      flex: 1,
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}>
-      <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-      <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-      <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
-    </View>
+      <View style={{padding: 50}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={text => this.setState({text})}
+          value={this.state.text}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text
+            .split(' ')
+            .map(word => word && '🇮🇳')
+            .join(' ')}
+        </Text>
+      </View>
     );
   }
 }
