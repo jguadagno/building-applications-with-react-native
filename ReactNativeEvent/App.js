1ac19e58cc8e650b94d7bf8a85014f77ac7309de
Guadagno <jguadagno@hotmail.com>
2020-02-27 13:36:24 -0700

    Hello World - LayoutFlexbox

--- ReactNativeEvent/App.js
+++ ReactNativeEvent/App.js
@@ -8,11 +8,15 @@ import {
 export default class FlexDirectionBasics extends Component {
   render() {
     return (
-    <View style={{flex: 1, flexDirection: 'row'}}>
-      <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-      <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-      <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
-    </View>
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      </View>
     );
   }
 }
