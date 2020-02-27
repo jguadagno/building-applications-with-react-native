71184a3c01dc3e396b8369fb40942cae19332b7d
Guadagno <jguadagno@hotmail.com>
2020-02-27 13:35:13 -0700

    Hello World - Layout

--- ReactNativeEvent/App.js
+++ ReactNativeEvent/App.js
@@ -8,12 +8,11 @@ import {
 export default class FlexDirectionBasics extends Component {
   render() {
     return (
-      // Try setting `flexDirection` to `column`.
-      <View style={{flex: 1, flexDirection: 'row'}}>
-        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
-      </View>
+    <View style={{flex: 1, flexDirection: 'row'}}>
+      <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+      <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+      <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+    </View>
     );
   }
 }
