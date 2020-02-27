d715182b87333c2d245a5a6d6c4790a2056b8078
Guadagno <jguadagno@hotmail.com>
2020-02-27 13:37:49 -0700

    Hello World - LayoutAlign

--- ReactNativeEvent/App.js
+++ ReactNativeEvent/App.js
@@ -8,15 +8,16 @@ import {
 export default class FlexDirectionBasics extends Component {
   render() {
     return (
-      <View style={{
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-      }}>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
-      </View>
+    <View style={{
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+      <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+      <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+    </View>
     );
   }
 }
