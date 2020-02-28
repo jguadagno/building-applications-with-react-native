f0cb8cd5e94bdbe9110bd5a9db2336f8f75fa23f
Guadagno <jguadagno@hotmail.com>
2020-02-28 16:48:42 -0700

    Hello World - Navigation - Functions

--- ReactNativeEvent/App.js
+++ ReactNativeEvent/App.js
@@ -1,98 +1,15 @@
-import React, {Component} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  ActivityIndicator,
-  Platform,
-  SectionList,
-} from 'react-native';
+import * as React from 'react';
+import {Button, View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-require('./services/array-extensions');
-
-export default class SectionListBasics extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-      isError: false,
-      userList: null,
-      groupedUserList: null,
-    };
-  }
-
-  render() {
-    if (this.state.isLoading) {
-      return (
-        <View style={[styles.container, styles.horizontal]}>
-          <ActivityIndicator size="large" />
-        </View>
-      );
-    }
-
-    if (this.state.isError) {
-      return (
-        <View style={[styles.container, styles.horizontal]}>
-          <Text style={styles.red}>Failed to connect to the service.</Text>
-        </View>
-      );
-    }
-
-    return (
-      <View style={styles.container}>
-        <SectionList
-          sections={this.state.groupedUserList}
-          keyExtractor={(item, index) => item + index}
-          renderItem={({item}) => (
-            <Text style={styles.item}>
-              {item.name.last}, {item.name.first}
-            </Text>
-          )}
-          renderSectionHeader={({section: {title}}) => (
-            <Text style={styles.header}>{title}</Text>
-          )}
-        />
-      </View>
-    );
-  }
-
-  componentDidMount() {
-    // If Android 10.0.2.2 is not working
-    // https://stackoverflow.com/questions/6760585/accessing-localhostport-from-android-emulator/56769746#56769746
-    var uri =
-      Platform.OS === 'android' ? 'http://10.0.2.2' : 'http://localhost';
-    uri = uri + ':3000/results';
-    fetch(uri)
-      .then(response => response.json())
-      .then(results => {
-        var sortedArray = results.map(x => x).sortByName();
-        sortedArray.forEach(user => {
-          user.lastInitial = user.name.last.charAt(0);
-        });
-
-        var sectionListArray = sortedArray
-          .map(x => x)
-          .groupByIntoArray(user => user.lastInitial);
-
-        this.setState(
-          {
-            isLoading: false,
-            userList: sortedArray,
-            groupedUserList: sectionListArray,
-          },
-          function() {},
-        );
-      })
-      .catch(error => {
-        this.setState({isLoading: false, isError: true});
-      });
-  }
-}
+import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
+  center: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
   },
   horizontal: {
     flexDirection: 'row',
@@ -102,13 +19,49 @@ const styles = StyleSheet.create({
   red: {
     color: 'red',
   },
-  item: {
-    backgroundColor: 'white',
-  },
-  header: {
-    fontSize: 32,
-    backgroundColor: 'azure',
-    paddingBottom: 5,
-    paddingTop: 10,
-  },
 });
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={[styles.center]}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen({navigation}) {
+  return (
+    <View style={styles.center}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
